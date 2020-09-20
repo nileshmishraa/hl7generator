2dbf95deb5d9f4a6e202483b9acc97aa14c2db2d
unknown <cnilesh97@gmail.com>
2020-09-20 20:56:48 +0530

    added new members

---
+++ src/main/java/com/hl7gen/hl7generator/model/Evn.java
@@ -0,0 +1,28 @@
+package com.hl7gen.hl7generator.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Evn {
+
+    @Id
+    private String triggerEvent;
+    private LocalDateTime localDateTime;
+    private LocalDateTime plannedEvent;
+    private String eventReasonCode;
+    private String operatorId;
+    private String eventOccurred;
+    private String eventFacility;
+
+}
--- src/main/java/com/hl7gen/hl7generator/model/Patient.java
+++ src/main/java/com/hl7gen/hl7generator/model/Patient.java
@@ -1,108 +1,58 @@
 package com.hl7gen.hl7generator.model;
 
+import com.sun.istack.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.UniqueConstraint;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Patient implements Serializable {
 
     @Id
     @Column(nullable = false)
     private Long id;
+    private String alternateId;
     private String name;
-    private String motherName;
-    private Date dob;
+    private String motherMaidenName;
+    private LocalDateTime dob;
     private String sex;
     private String alias;
-    private String phone;
+    private String race;
+    private String address;
+    private String countryCode;
+    private Long phoneHome;
+    private Long phoneBusiness;
+    private String primaryLang;
+    private String maritalStatus;
+    private String religion;
+    private String patientAccountNo;
+    @Column(nullable = false, updatable = false)
+    private Long ssn;
     private String email;
+    private String driverLicenseNo;
+    private String motherIdentifier;
+    private String ethnicGroup;
+    private String birthPlace;
+    private String multipleBirthIndicator;
+    private String birthOrder;
+    private String citizenship;
+    private String veteranMilitaryStatus;
+    private String nationality;
+    private LocalDateTime deathTime;
+    private String patientDeathIndicator;
 
-    public Patient() {
-    }
 
-    public Patient(Long id, String name, String motherName, Date dob, String sex, String alias, String phone, String email) {
-        this.id = id;
-        this.name = name;
-        this.motherName = motherName;
-        this.dob = dob;
-        this.sex = sex;
-        this.alias = alias;
-        this.phone = phone;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Patient setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Patient setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public String getMotherName() {
-        return motherName;
-    }
-
-    public Patient setMotherName(String motherName) {
-        this.motherName = motherName;
-        return this;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-
-    public Patient setDob(Date dob) {
-        this.dob = dob;
-        return this;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public Patient setSex(String sex) {
-        this.sex = sex;
-        return this;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public Patient setAlias(String alias) {
-        this.alias = alias;
-        return this;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public Patient setPhone(String phone) {
-        this.phone = phone;
-        return this;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Patient setEmail(String email) {
-        this.email = email;
-        return this;
-    }
 }
